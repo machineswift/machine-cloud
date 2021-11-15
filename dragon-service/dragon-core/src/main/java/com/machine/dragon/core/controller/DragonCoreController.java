6b1035c38e21bd7bcc139da919e18eb7c3053c3d
machine <machineswift@163.com>
2021-11-15 23:46:27 +0800

    Nacos config

--- dragon-service/dragon-core/src/main/java/com/machine/dragon/core/controller/DragonCoreController.java
+++ dragon-service/dragon-core/src/main/java/com/machine/dragon/core/controller/DragonCoreController.java
@@ -1,11 +1,13 @@
 package com.machine.dragon.core.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
+@RefreshScope
 @RestController
 @RequestMapping("core")
 public class DragonCoreController {
--- dragon-service/dragon-demo/src/main/java/com/machine/dragon/demo/controller/DragonDemoController.java
+++ dragon-service/dragon-demo/src/main/java/com/machine/dragon/demo/controller/DragonDemoController.java
@@ -2,15 +2,24 @@ package com.machine.dragon.demo.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @Slf4j
+@RefreshScope
 @RestController
 @RequestMapping("demo")
 public class DragonDemoController {
+
+    @Value("${machine}")
+    private String machine;
+
+    @Value("${machine1}")
+    private String machine1;
 
     @Autowired
     private RestTemplate restTemplate;
@@ -18,8 +27,7 @@ public class DragonDemoController {
     @GetMapping("detail")
     public String detail() {
         String helloWorld = restTemplate.getForObject("http://dragon-core/core/detail", String.class);
-        //String helloWorld = restTemplate.getForObject("http://localhost:8080/core/detail", String.class);
-        log.info(helloWorld);
+        log.info(helloWorld + machine + machine1);
         return helloWorld;
     }
 }
